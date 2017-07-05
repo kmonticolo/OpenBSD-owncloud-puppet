c736bd9d05ef87d7f35e38e0dfaaa3b816d7b74e
Kamil Monticolo <kamil.monticolo@bolero.se>
2017-07-05 08:28:27 +0200

    remove version from stdlib install

--- site.pp
+++ site.pp
@@ -1,6 +1,6 @@
 include stdlib
 Exec { path => [  '/bin/', '/sbin/', '/usr/bin/' , '/usr/local/bin/', '/usr/sbin/' ] }
-# puppet module install puppetlabs-stdlib --version 4.15.0
+# puppet module install puppetlabs-stdlib 
 # os specific stuff
 $arch=$::facts['processors']['isa']
 $mirror = "http://ftp.icm.edu.pl/pub/OpenBSD/${::operatingsystemrelease}/"
