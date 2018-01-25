4bde2098d3e59226ddf06ba221cb9ae258d408c4
Kamil Monticolo <kmonticolo@gmail.com>
2018-01-26 00:42:43 +0100

    add error at step

--- test.sh
+++ test.sh
@@ -1,79 +1,79 @@
 #!/bin/sh
 # 14 quick and dirty tests
   --------------------------------
-echo 1 OPEN PORTS 443 and 5432 --------------------------------
-netstat -aln|grep LIST  |grep \*.443 || err_flag=1
-netstat -aln|grep LIST  |grep 127.0.0.1.5432 || err_flag=1
+ $((i=i+1)) OPEN PORTS 443 and 5432 --------------------------------
+netstat -aln|grep LIST  |grep \*.443 || err_flag=$i
+netstat -aln|grep LIST  |grep 127.0.0.1.5432 || err_flag=$i
 
 echo
 
 # todo disable ipv6
 
-echo 2 website  --------------------------------
+echo $((i=i+1))  website  --------------------------------
 #curl -vk https://192.168.1.131/owncloud/index.php
 #curl -vk https://192.168.1.131/owncloud/index.php/login
 
-curl -svk https://192.168.1.131/owncloud/index.php 2>x; grep owncloud x || err_flag=1; rm x
+curl -svk https://192.168.1.131/owncloud/index.php 2>x; grep owncloud x || err_flag=$i; rm x
 echo
 
-echo 3 cron  --------------------------------
+echo $((i=i+1)) cron  --------------------------------
 
-crontab -u www -l|grep owncloud || err_flag=1
+crontab -u www -l|grep owncloud || err_flag=$i
 
-ls -l /var/www/owncloud/cron.php || err_flag=1
+ls -l /var/www/owncloud/cron.php || err_flag=$i
 echo
 
 # chroot ?
 
-echo 4 proc  --------------------------------
+echo $((i=i+1)) proc  --------------------------------
 
-ps auxw|grep httpd || err_flag=1
+ps auxw|grep httpd || err_flag=$i
 
-ps auxw|grep php || err_flag=1
+ps auxw|grep php || err_flag=$i
 
 echo
 # todo grep for versions
-echo 5 packages  --------------------------------
-pkg_info |grep -E '(httpd|php|postgres|owncloud)' || err_flag=1
+echo $((i=i+1)) packages  --------------------------------
+pkg_info |grep -E '(httpd|php|postgres|owncloud)' || err_flag=$i
 echo
 
-echo 6 php version  --------------------------------
-ls -l `which php` || err_flag=1
+echo $((i=i+1)) php version  --------------------------------
+ls -l `which php` || err_flag=$i
 echo
 
-echo 7 mount point /var with nodev  --------------------------------
-mount |grep "/var"|grep nodev || err_flag=1
+echo $((i=i+1)) mount point /var with nodev  --------------------------------
+mount |grep "/var"|grep nodev || err_flag=$i
 echo
 
 # database todo
 # log to db and check owncloud db
     
-echo 8 cert ssl  --------------------------------
-file /etc/ssl/*.crt|grep PEM || err_flag=1
+echo $((i=i+1)) cert ssl  --------------------------------
+file /etc/ssl/*.crt|grep PEM || err_flag=$i
 echo
 
-echo 9 ssl key  --------------------------------
-head -1 /etc/ssl/private/*key|grep PRIVATE || err_flag=1
+echo $((i=i+1)) ssl key  --------------------------------
+head -1 /etc/ssl/private/*key|grep PRIVATE || err_flag=$i
 
 echo
-echo 10 chroot files  --------------------------------
-file /var/www/usr/share/locale/UTF-8/LC_CTYPE |grep Citrus || err_flag=1
+echo $((i=i+1)) chroot files  --------------------------------
+file /var/www/usr/share/locale/UTF-8/LC_CTYPE |grep Citrus || err_flag=$i
 echo
 
-echo 11 system users  --------------------------------
-id _postgresql  || err_flag=1
+echo $((i=i+1)) system users  --------------------------------
+id _postgresql  || err_flag=$i
 echo
 
-echo 12 x11 dir  --------------------------------
+echo $((i=i+1)) x11 dir  --------------------------------
 
-ls -ld /usr/X11R6/bin/ || err_flag=1
+ls -ld /usr/X11R6/bin/ || err_flag=$i
 echo
 
-echo 13 symlinks php  --------------------------------
-for i in bz2 curl gd intl mcrypt pdo_pgsql pgsql zip; do ls /etc/php-7.0/"$i".ini || err_flag=1;done; echo $?
+echo $((i=i+1)) symlinks php  --------------------------------
+for i in bz2 curl gd intl mcrypt pdo_pgsql pgsql zip; do ls /etc/php-7.0/"$i".ini || err_flag=$i ;done; echo $?
 echo
 
-echo 14 php fpm proc  --------------------------------
-ps aux|grep php-fpm|grep 7.0 || err_flag=1
+echo $((i=i+1)) php fpm proc  --------------------------------
+ps aux|grep php-fpm|grep 7.0 || err_flag=$i
 
-[ $err_flag ] && exit 1
+[ $err_flag ] && { echo ERROR at step $i; exit 1 }
